b2eb989fdb07f9223a575f058436996e56308473
Ilya Emelyanov <baralgin38@yandex.ru>
2021-08-23 01:37:16 +0300

    Учит шаблон выводить данные о маршруте
    Компонент с информацией о маршруте теперь может выводить моковые данные:
    - Города путешествия
    - Даты и месяц
    - Общая стоимость

--- src/view/trip-info.js
+++ src/view/trip-info.js
@@ -1,13 +1,31 @@
-export  const getTripInfoTemplate = () => (
-  `<section class="trip-main__trip-info  trip-info">
-  <div class="trip-info__main">
-    <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+const printCities = (cities) => {
+  if (cities.length <= 3) {
+    return cities.join(' — ');
+  } else if (cities.length > 3) {
+    return `${cities[0]} — ... — ${cities[cities.length - 1]}`;
+  }
+};
 
-    <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+const printDates = (dates, months) => {
+  if (months.length > 1) {
+    return `${months[0]} ${dates[0]}&nbsp;&mdash;&nbsp;${months[months.length -1]} ${dates[dates.length - 1]}`;
+  } else {
+    return `${months[0]} ${dates[0]}&nbsp;&mdash;&nbsp;${dates[dates.length - 1]}`;
+  }
+};
+
+export  const getTripInfoTemplate = (data) => {
+  const {cities, dates, months, totalPrice} = data;
+
+  return `<section class="trip-main__trip-info  trip-info">
+  <div class="trip-info__main">
+    <h1 class="trip-info__title">${printCities(cities)}</h1>
+
+    <p class="trip-info__dates">${printDates(dates, months)}</p>
   </div>
 
   <p class="trip-info__cost">
-    Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+    Total: &euro;&nbsp;<span class="trip-info__cost-value">${totalPrice}</span>
   </p>
-</section>`
-);
+</section>`;
+};
