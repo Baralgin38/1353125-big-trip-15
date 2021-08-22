162c03a64731ab90599bbb877d60979c6124208d
Ilya Emelyanov <baralgin38@yandex.ru>
2021-08-23 01:41:52 +0300

    Удаляет не нужные утилитарные функции

--- src/util.js
+++ src/util.js
@@ -2,6 +2,7 @@ const MINUTES_PER_HOUR = 60;
 const MINUTES_PER_DAY = 1440;
 const HOURS_PER_DAY = 24;
 
+
 const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
